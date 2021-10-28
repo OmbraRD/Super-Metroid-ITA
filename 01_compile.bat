fd7c0ec7e5b6d14e96b0651013d110d030a357cc
DESKTOP-NK1MA93\PC <dodinazareno@gmail.com>
2021-10-28 17:08:07 +0200

    added and clear up some data

--- 01_compile.bat
+++ 01_compile.bat
@@ -35,11 +35,15 @@ echo Compressing Tilemaps...
 
 echo.
 echo Encoding Audio Data...
-%BRRENC% -sb14400 %AUDIO_ITA%\2E2F84_intro_1a_ITA.wav %AUDIO_ITA%\2E2F84_intro_1a_ITA.brr 
-%BRRENC% -sb14400 %AUDIO_ITA%\2E5558_intro_1b_ITA.wav %AUDIO_ITA%\2E5558_intro_1b_ITA.brr 
-%BRRENC% -sb14400 %AUDIO_ITA%\2E7ADF_intro_2a_ITA.wav %AUDIO_ITA%\2E7ADF_intro_2a_ITA.brr 
-%BRRENC% -sb14400 %AUDIO_ITA%\2E941D_intro_2b_ITA.wav %AUDIO_ITA%\2E941D_intro_2b_ITA.brr 
+%BRRENC% -sb14400 %AUDIO_ITA%\Infrid_Audio\2E2F84_intro_1a_ITA.wav %AUDIO_ITA%\Infrid_Audio\2E2F84_intro_1a_ITA.brr 
+%BRRENC% -sb14400 %AUDIO_ITA%\Infrid_Audio\2E5558_intro_1b_ITA.wav %AUDIO_ITA%\Infrid_Audio\2E5558_intro_1b_ITA.brr 
+%BRRENC% -sb14400 %AUDIO_ITA%\Infrid_Audio\2E7ADF_intro_2a_ITA.wav %AUDIO_ITA%\Infrid_Audio\2E7ADF_intro_2a_ITA.brr 
+%BRRENC% -sb14400 %AUDIO_ITA%\Infrid_Audio\2E941D_intro_2b_ITA.wav %AUDIO_ITA%\Infrid_Audio\2E941D_intro_2b_ITA.brr 
 
+%BRRENC% -sb14400 %AUDIO_ITA%\Dubber_audio\metroid_l_ultimo_metroid.wav %AUDIO_ITA%\Dubber_audio\metroid_l_ultimo_metroid.brr 
+%BRRENC% -sb14400 %AUDIO_ITA%\Dubber_audio\metroid_vive_in_cattivita.wav %AUDIO_ITA%\Dubber_audio\metroid_vive_in_cattivita.brr 
+%BRRENC% -sb14400 %AUDIO_ITA%\Dubber_audio\metroid_nella_galassia.wav %AUDIO_ITA%\Dubber_audio\metroid_nella_galassia.brr 
+%BRRENC% -sb14400 %AUDIO_ITA%\Dubber_audio\metroid_regna_la_pace.wav %AUDIO_ITA%\Dubber_audio\metroid_regna_la_pace.brr 
 
 echo.
 echo Applying hacks...
